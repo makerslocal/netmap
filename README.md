8ae039c3407295db355f5771d8d57259068e32be
Hunter Fuller <hfuller@pixilic.com>
2015-01-21 01:05:19 -0600

    initial commit with some stuff

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # netmap
-View our network as a subway map
+View our network as a subway map.
+
+You can run setup.sh to grab all the prerequisites for this.
+
+Based on jquery and https://github.com/techbubble/subwayMap
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget "https://github.com/techbubble/subwayMap/raw/master/jquery.subwayMap-0.5.0.js" -O "jquery.subwayMap.js"
+wget "http://code.jquery.com/jquery-1.9.0.min.js" -O "jquery.min.js"
+
+echo "k done"
